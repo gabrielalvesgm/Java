1f51cd38aa25f300af4a07017fd01e7572dbe67d
GabrielAlvesGM <lgabrielalvesgm@gmail.com>
2025-02-10 15:28:26 -0300

    First commit

---
+++ FirstClass.java
@@ -0,0 +1,9 @@
+public class FirstClass {
+    public static void main(String[] args) {
+        double teste = 20.00;
+        double teste2 = 80.00;
+        double resultadoTeste = teste + teste2 * 100;
+        double divisor = resultadoTeste % 40.00;
+        boolean istrue = true;
+    }
+}
